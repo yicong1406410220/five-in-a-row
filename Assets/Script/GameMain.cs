174b366bc48c1f1e43c657b27c154a6a5fa2fa17
yicong1406410220 <azazaz123456@foxmail.com>
2019-10-21 23:14:25 +0800

    UI系统

---
+++ Assets/Script/GameMain.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour {
+	public static string id = "";
+
+
+	// Use this for initialization
+	void Start () {
+		//网络监听
+		NetManager.AddEventListener(NetManager.NetEvent.Close, OnConnectClose);
+		NetManager.AddMsgListener("MsgKick", OnMsgKick);
+		//初始化
+		PanelManager.Init();
+		//打开登陆面板
+		PanelManager.Open<LoginPanel>();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		NetManager.Update();
+	}
+
+	//关闭连接
+	void OnConnectClose(string err){
+		Debug.Log("断开连接");
+	}
+
+	//被踢下线
+	void OnMsgKick(MsgBase msgBase){
+		PanelManager.Open<TipPanel>("被踢下线");
+	}
+}
---
+++ Assets/Script/panel/Login/LoginPanel.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginPanel : BasePanel {
+	//账号输入框
+	private InputField idInput;
+	//密码输入框
+	private InputField pwInput;
+	//登陆按钮
+	private Button loginBtn;
+	//注册按钮
+	private Button regBtn;
+
+	//初始化
+	public override void OnInit() {
+		skinPath = "LoginPanel";
+		layer = PanelManager.Layer.Panel;
+	}
+
+	//显示
+	public override void OnShow(params object[] args) {
+		//寻找组件
+		idInput = skin.transform.Find("IdInput").GetComponent<InputField>();
+		pwInput = skin.transform.Find("PwInput").GetComponent<InputField>();
+		loginBtn = skin.transform.Find("LoginBtn").GetComponent<Button>();
+		regBtn = skin.transform.Find("RegisterBtn").GetComponent<Button>();
+		//监听
+		loginBtn.onClick.AddListener(OnLoginClick);
+		regBtn.onClick.AddListener(OnRegClick);
+		//网络协议监听
+		NetManager.AddMsgListener("MsgLogin", OnMsgLogin);
+		//网络事件监听
+		NetManager.AddEventListener(NetManager.NetEvent.ConnectSucc, OnConnectSucc);
+		NetManager.AddEventListener(NetManager.NetEvent.ConnectFail, OnConnectFail);
+		//连接服务器
+		NetManager.Connect("127.0.0.1", 8888);
+	}
+
+	//关闭
+	public override void OnClose() {
+		//网络协议监听
+		NetManager.RemoveMsgListener("MsgLogin", OnMsgLogin);
+		//网络事件监听
+		NetManager.RemoveEventListener(NetManager.NetEvent.ConnectSucc, OnConnectSucc);
+		NetManager.RemoveEventListener(NetManager.NetEvent.ConnectFail, OnConnectFail);
+	}
+
+
+	//连接成功回调
+	void OnConnectSucc(string err){
+		Debug.Log("OnConnectSucc");
+	}
+
+	//连接失败回调
+	void OnConnectFail(string err){
+		PanelManager.Open<TipPanel>(err);
+	}
+
+
+
+	//当按下注册按钮
+	public void OnRegClick() {
+		PanelManager.Open<RegisterPanel>();
+	}
+		
+
+
+	//当按下登陆按钮
+	public void OnLoginClick() {
+		//用户名密码为空
+		if (idInput.text == "" || pwInput.text == "") {
+			PanelManager.Open<TipPanel>("用户名和密码不能为空");
+			return;
+		}
+		//发送
+		MsgLogin msgLogin = new MsgLogin();
+		msgLogin.id = idInput.text;
+		msgLogin.pw = pwInput.text;
+		NetManager.Send(msgLogin);
+	}
+
+	//收到登陆协议
+	public void OnMsgLogin (MsgBase msgBase) {
+		MsgLogin msg = (MsgLogin)msgBase;
+		if(msg.result == 0){
+			Debug.Log("登陆成功");
+			//进入游戏
+			//添加坦克
+			//GameObject tankObj = new GameObject("myTank");
+			//CtrlTank ctrlTank = tankObj.AddComponent<CtrlTank>();
+			//ctrlTank.Init("tankPrefab");
+			////设置相机
+			//tankObj.AddComponent<CameraFollow>();
+			//设置id
+			GameMain.id = msg.id;
+			//关闭界面
+			Close();
+		}
+		else{
+			PanelManager.Open<TipPanel>("登陆失败");
+		}
+	}
+
+}
